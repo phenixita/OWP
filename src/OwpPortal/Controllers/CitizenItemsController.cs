a201aa093a3aa369bc384bbe55fad6338f64be8f
Toby Couchman <toby.couchman@microsoft.com>
2019-10-15 17:37:15 +0100

    Auth fixes

--- src/OwpPortal/Controllers/CitizenItemsController.cs
+++ src/OwpPortal/Controllers/CitizenItemsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using owp_web.Models;
 
 namespace owp_web.Controllers
 {
+    [AllowAnonymous]
     public class CitizenItemsController : Controller
     {
         private readonly OwpContext _context;
