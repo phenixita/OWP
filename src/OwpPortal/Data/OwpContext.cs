3cacc99f2182cf705a2f33ce25b93e287167d9bf
Carlo Kuip <cakuip@microsoft.com>
2019-10-16 22:46:05 +0200

    couple of pieces didn't come along from stash, now added DI to app configuration for IOwpContext

--- src/OwpPortal/Data/OwpContext.cs
+++ src/OwpPortal/Data/OwpContext.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using owp_web.Data;
 
 namespace owp_web.Models
 {
-    public class OwpContext : DbContext
+    public class OwpContext : DbContext, IOwpContext
     {
         public OwpContext (DbContextOptions<OwpContext> options)
             : base(options)
@@ -15,5 +18,35 @@ namespace owp_web.Models
         }
 
         public DbSet<owp_web.Models.WorkItem> WorkItem { get; set; }
+
+        Task<EntityEntry> IDbContext.AddAsync(object entity, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<EntityEntry<TEntity>> IDbContext.AddAsync<TEntity>(TEntity entity, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<TEntity> IDbContext.FindAsync<TEntity>(params object[] keyValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<object> IDbContext.FindAsync(Type entityType, object[] keyValues, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<TEntity> IDbContext.FindAsync<TEntity>(object[] keyValues, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<object> IDbContext.FindAsync(Type entityType, params object[] keyValues)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
