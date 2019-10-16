0884e4445c2f2d08d40f44553ea97db6cf8760a7
Yael Barteau <yabartea@microsoft.com>
2019-10-16 11:43:32 +0100

    SImplify enum drop down lists

--- src/OwpPortal/Models/WorkItemViewModel.cs
+++ src/OwpPortal/Models/WorkItemViewModel.cs
@@ -10,34 +10,6 @@ namespace owp_web.Models
     {
         #region Public Properties
         public WorkItem WorkItem { get; set; }
-     
-        public List<SelectListItem> WorkItemStatuses
-        {
-            get
-            {
-                var listItems = new List<SelectListItem>();
-                var items = Enum.GetValues(typeof(WorkItemStatus));
-
-                foreach (var item in items)
-                {
-                    listItems.Add(new SelectListItem(item.ToString(), item.ToString()));
-                }
-                return listItems;
-            }
-        }
-        public List<SelectListItem> WorkItemTypes
-        {
-            get
-            {
-                var listItems = new List<SelectListItem>();
-                var items = Enum.GetValues(typeof(WorkItemTypes)).Cast<WorkItemTypes>();
-                foreach(var item in items)
-                {
-                    listItems.Add(new SelectListItem(item.ToString(), item.ToString()));
-                }
-                return listItems;
-            }
-        }
         #endregion
 
         #region Constructors
