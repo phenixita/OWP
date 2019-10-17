062a59aa8a93776d794926ca9b1e8295a5482946
Tiago Antunes <tiantun@microsoft.com>
2019-10-17 11:57:01 +0100

    Add search fields for columns

--- src/OwpPortal/wwwroot/js/TechManagerSite.js
+++ src/OwpPortal/wwwroot/js/TechManagerSite.js
@@ -1,3 +1,17 @@
 ﻿$(document).ready(function () {
-    $('#workitems').DataTable();
+    $('#workitems thead tr:eq(1) th').each(function (i) {
+        $('input', this).on('keyup change', function () {
+            if (table.column(i).search() !== this.value) {
+                table
+                    .column(i)
+                    .search(this.value)
+                    .draw();
+            }
+        });
+    });
+
+    var table = $('#workitems').DataTable({
+        orderCellsTop: true,
+        fixedHeader: true
+    });
 });
