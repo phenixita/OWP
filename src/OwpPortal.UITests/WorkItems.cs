d3c1cc50b0e883bb99c3230d27a6e78b7a3db647
Carlo Kuip <cakuip@microsoft.com>
2019-10-17 15:24:40 +0200

    Fixed UI test

--- src/OwpPortal.UITests/WorkItems.cs
+++ src/OwpPortal.UITests/WorkItems.cs
@@ -23,21 +23,22 @@ namespace OwpPortal.UITests
         }
 
         [Theory]
-        [InlineData(1, "Reading", "road is broken")]
-        [InlineData(2, "Reading", "paving broken")]
-        [InlineData(3, "London", "bridge broken")]
-        public void CanCreateIssue(int WorkItemType, string workItemAdress, string WorkItemDescription)
+        [InlineData("WorkItemType1", "Reading", "road is broken")]
+        [InlineData("WorkItemType2", "Reading", "paving broken")]
+        [InlineData("WorkItemType3", "London", "bridge broken")]
+        public void CanCreateIssue(string WorkItemType, string workItemAdress, string WorkItemDescription)
         {
             // Arrange
             driver.Manage().Window.Maximize();
             driver.Url = url;
-            IWebElement WorkItemTypeRadioBtn = driver.FindElement(By.XPath($".//*[@id='{CreateItemFeatureSelectors.WorkItem_Type_Id}'and @value='{WorkItemType}']"));
+            //IWebElement WorkItemTypeSelectionBtn = driver.FindElement(By.Id(WorkItemType));
+            IWebElement WorkItemTypeSelectionBtn = driver.FindElement(By.XPath($".//*[@id='{WorkItemType}']"));
             IWebElement WorkItemAdressField = driver.FindElement(By.XPath($".//*[@id='{CreateItemFeatureSelectors.WorkItem_Adress_Id}']"));
             IWebElement WorkItemDescriptionField = driver.FindElement(By.XPath($".//*[@id='{CreateItemFeatureSelectors.WorkItem_Description_Id}']"));
             IWebElement SubmitButton = driver.FindElement(By.XPath($".//*[@value='{CreateItemFeatureSelectors.SubmitButton_Value}' and @type='submit']"));
 
             // Act
-            WorkItemTypeRadioBtn.Click();
+            WorkItemTypeSelectionBtn.Click();
             WorkItemAdressField.SendKeys(workItemAdress);
             WorkItemDescriptionField.SendKeys(WorkItemDescription);
             SubmitButton.Click();
