3f03182045b0abdbddf115fe2e4d2b0791b921e4
Michele Ferracin <miferrac@microsoft.com>
2020-09-23 11:41:23 +0200

    polishing scripts

--- .ci/database/createMigrationScript.ps1
+++ .ci/database/createMigrationScript.ps1
@@ -1,12 +1,69 @@
-# .\Deploy-Tools\SqlPackage\sqlpackage.exe /Action:Script /SourceFile:"xxxx.dacpac" /TargetServerName:"xxx" /TargetDatabaseName:"databaseDaCatena" /DeployScriptPath:"c:\temp\dlm\migscript0000.sql" /DeployReportPath:"C:\temp\dlm\MigrationReport.xml" /Profile:"Database.publish.xml"
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $sqlpackageExeContainingFolder, # Custom directory that contains sqlpackage.exe.
+    [Parameter()]
+    [string]
+    $repoRootFolder, # Root folder of the repository.
+    [Parameter()]
+    [string]
+    $dacpacSourceFile, # Full path or relative to root folder of the repo.
+    [Parameter()]
+    [string]
+    $TargetServerName,  
+    [Parameter()]
+    [string]
+    $targetDatabaseName, 
+    [Parameter()]
+    [string]
+    $DeployScriptPath, 
+    [Parameter()] 
+    [string]
+    $DeployReportPath,
+    [Parameter()] 
+    [string]
+    $ProfilePath # Full path or relative to root folder of the repo.
+)
+
+if (-not(Test-Path $repoRootFolder)) {
+    Write-Error "Repo root folder [$repoRootFolder] not found!"
+    exit 1
+}
+ 
+$sqlpackageFullPath = "sqlpackage.exe"
+if ($sqlpackageExeContainingFolder) {
+    $sqlpackageFullPath = Join-Path -Path $sqlpackageExeContainingFolder -ChildPath "sqlpackage.exe"
+
+    if (-not(Test-Path -Path $sqlpackageFullPath)) {
+        Write-Error "sqlpackage.exe not found in $sqlpackageFullPath"
+        exit 1
+    }
+}
+else {
+    Write-Host "Searching for sqlpackage.exe in default locations..."
+    $sqlpackageFullPath = (Get-ChildItem -Path "$env:SystemDrive\Program Files (x86)\" -Filter "sqlpackage.exe" -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1 | select FullName).FullName
+    if ($sqlpackageFullPath) {
+        Write-Host "Found $sqlpackageFullPath"
+    }
+    else {
+        Write-Error "SqlPackage.exe not found!"
+        exit 1
+    }
+}
 
 
-& "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\150\sqlpackage.exe" `
+Push-Location $repoRootFolder
+
+& $sqlpackageFullPath `
     /Action:Script `
-    /SourceFile:"src\OwpPortal.Database\bin\Release\OwpPortal.Database.dacpac" `
-    /TargetServerName:"localhost" `
-    /TargetDatabaseName:"model" `
-    /DeployScriptPath:"c:\temp\dlm\migscript0000.sql" `
-    /DeployReportPath:"C:\temp\dlm\MigrationReport.xml" `
-    /Profile:".ci\databasePublishProfile.xml" 
+    /SourceFile:$dacpacSourceFile `
+    /TargetServerName:$targetServerName `
+    /TargetDatabaseName:$targetDatabaseName `
+    /DeployScriptPath:$DeployScriptPath `
+    /DeployReportPath:$DeployReportPath `
+    /Profile:$ProfilePath
 
+Pop-Location
+
+Write-Host "Done!" -ForegroundColor Green
--- .ci/database/sanitizeMigrationScript.ps1
+++ .ci/database/sanitizeMigrationScript.ps1
@@ -1,19 +1,32 @@
-$filePath = "C:\temp\dlm\migscript0000.sql"
-$file = Get-Content -Path $filePath -Raw
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $filePath
+)
+ 
+$file = Get-Content -Path $filePath -Raw -ErrorAction Stop
 
-# Remove USE statements
+
+Write-Host "Remove USE statements"
 $regEx = [regex]'USE \[*.*\];'
 $match = $regEx.Match($file)
 $index = $match.Index + $match.Length
 
-# Clean everything from beginning of file to USE [database] included.
+Write-Host "Clean everything from beginning of file to USE [database] included."
 $file = $file.Remove(0, $index)
 
-# Re-insert useful statements.
-$file = $file.Insert(0, "SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
-SET NUMERIC_ROUNDABORT OFF;")
+$defaultStatements = "SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+SET NUMERIC_ROUNDABORT OFF;"
 
-# Clean other USE statements
-$file = $file -replace $regEx, ''
+if (-not($file.StartsWith($defaultStatements))) {
+    $file = $file.Insert(0, $defaultStatements)
 
-Set-Content -Path $filePath -Value $file
+    Write-Host "Clean other USE statements"
+    $file = $file -replace $regEx, ''
+
+    Write-Host "Writing file"
+    Set-Content -Path $filePath -Value $file
+}
+
+Write-Host "Sanitized!" -ForegroundColor Green
